f26dae4e2454732752a19019c6e389c4c4db4e6b
Rachael Rice <Rachnicrice@gmail.com>
2020-01-06 21:44:38 -0800

    Setting up for lab-01

---
+++ basics/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("I am working");
+  }
+
+  public static String pluralize (String animal, int num) {
+
+    return "A string";
+  }
+
+  public static String flipNHeads (int n) {
+
+    return "heads or tails";
+  }
+
+  public static String clock () {
+
+    return "the time";
+  }
+
+}
