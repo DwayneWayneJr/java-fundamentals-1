b6f200c2b11003de3e6fc002816da6df426e409e
Rachael Rice <Rachnicrice@gmail.com>
2020-01-14 09:24:26 -0800

    Added test for constructor function

--- inheritance/src/test/java/inheritance/RestaurantTest.java
+++ inheritance/src/test/java/inheritance/RestaurantTest.java
@@ -1,12 +1,23 @@
 package inheritance;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.naming.spi.ResolveResult;
+
 public class RestaurantTest {
-    
+    Restaurant input;
+
+    @Before
+    public void initialize () throws Exception {
+        input = new Restaurant("My Lil' Cube", 4, "$$");
+    }
 
     @Test
-    public static void testConstructorFunction () {
-
+    public void testConstructorFunction () {
+        Assert.assertEquals("My Lil' Cube", input.name);
+        Assert.assertEquals(4, input.rating);
+        Assert.assertEquals("$$", input.price);
     }
 }
